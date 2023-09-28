adc6a2bcb7293ab5442d7de760c42073895c0f3f
Sharad Agarwal <obviouslysharad@gmail.com>
2023-09-28 22:34:51 +0530

    Bind store

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="home"></div>
+  <p>{{ $store.state.name }}</p>
 </template>
 
 <script>
 export default {
   name: "Home",
-  components: {},
+  components: {
+  },
 };
 </script>
